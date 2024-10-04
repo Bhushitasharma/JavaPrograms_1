af090efbe69c2bc92ca91cbfdd53303a563f5f73
BHUSHITA SHARMA <bhusshar@adobe.com>
2024-10-04 06:59:02 +0530

    Initial commit

---
+++ src/Oct/ex_03102024/Lab003.java
@@ -0,0 +1,18 @@
+package Oct.ex_03102024;
+
+public class Lab003 {public static void main(String[] args){
+    int x;
+    int y;
+    int z;
+    int i;
+    int u;
+    x= 6;
+    y=12;
+    z=x+y;
+    i=x-y;
+    u=x*y;
+System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+}
+}
